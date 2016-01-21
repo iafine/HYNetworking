3f120bebab9270126776a3142ad39097a061cdad
castial <897257524@qq.com>
2016-01-21 09:45:17 +0800

    Initial Commit

---
+++ HYNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYNetwork
+//
+//  Created by hyyy on 16/1/21.
+//  Copyright © 2016年 hyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYNetwork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYNetwork
+//
+//  Created by hyyy on 16/1/21.
+//  Copyright © 2016年 hyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
